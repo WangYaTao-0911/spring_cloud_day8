bbe7b812446b03f3c76bf1921f675e5967fbe865
WangYaTao-0911 <56395093+WangYaTao-0911@users.noreply.github.com>
2020-04-26 09:03:46 +0800

    config 配置初始化添加注释

--- config_client/src/main/java/com/wyt/ConfigClientApplication.java
+++ config_client/src/main/java/com/wyt/ConfigClientApplication.java
@@ -4,6 +4,7 @@ package com.wyt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动类
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
--- config_server/src/main/java/com/wyt/ConfigServerApplication.java
+++ config_server/src/main/java/com/wyt/ConfigServerApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/***
+ * config服务注解
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
+    /***
+     * main主函数启动类
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
     }
